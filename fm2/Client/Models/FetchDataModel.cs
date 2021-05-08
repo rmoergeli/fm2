a2dc51d0fd6fc92080dd7ee1f36f7bfa084d7cdc
rmoergeli <rene.moergeli@windowslive.com>
2021-05-08 10:59:42 +0200

    Add project files.

---
+++ fm2/Client/Models/FetchDataModel.cs
@@ -0,0 +1,28 @@
+﻿using fm2.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace fm2.Client.Models
+{
+    public interface IFetchDataModel
+    {
+        Task<WeatherForecast[]> RetrieveForecastsAsync();
+    }
+    public class FetchDataModel : IFetchDataModel
+    {
+        private HttpClient _http;
+        public FetchDataModel(HttpClient http)
+        {
+            _http = http;
+        }
+        public async Task<WeatherForecast[]> RetrieveForecastsAsync()
+        {
+            return await _http.GetFromJsonAsync<WeatherForecast[]>("api/SampleData/WeatherForecasts");
+        }
+    }
+}
---
+++ fm2/Client/ViewModels/FetchDataViewModel.cs
@@ -0,0 +1,42 @@
+﻿using fm2.Client.Models;
+using fm2.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace fm2.Client.ViewModels
+{
+    public interface IFetchDataViewModel
+    {
+        WeatherForecast[] WeatherForecasts { get; set; }
+        Task RetrieveForecastsAsync();
+        Task OnInitializedAsync();
+    }
+    public class FetchDataViewModel : IFetchDataViewModel
+    {
+        private WeatherForecast[] _weatherForecasts;
+        private IFetchDataModel _fetchDataModel;
+        public WeatherForecast[] WeatherForecasts
+        {
+            get => _weatherForecasts;
+            set => _weatherForecasts = value;
+        }
+        public FetchDataViewModel(IFetchDataModel fetchDataModel)
+        {
+            Console.WriteLine("FetchDataViewModel Constructor Executing");
+            _fetchDataModel = fetchDataModel;
+        }
+
+        public async Task RetrieveForecastsAsync()
+        {
+            _weatherForecasts = await _fetchDataModel.RetrieveForecastsAsync();
+            Console.WriteLine("FetchDataViewModel Forecasts Retrieved");
+        }
+
+        public async Task OnInitializedAsync()
+        {
+            _weatherForecasts = await _fetchDataModel.RetrieveForecastsAsync();
+        }
+    }
+}
